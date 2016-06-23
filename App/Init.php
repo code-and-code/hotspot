ef12b6c0afabe044d8efab4ba241d6d096d1d2ef
Angelo Biscola <angelo.biscola@gmail.com>
2016-06-23 17:30:10 -0300

    Fix

--- App/Init.php
+++ App/Init.php
@@ -15,21 +15,20 @@ class Init extends Bootstrap
         $ar['home.service']           = ['route' => '/servico',                             'controller' => 'SiteController',       'action' => 'servico'                       ];
 
 
-
         $ar['admin']                   = ['route' => '/admin',                              'controller' => 'AdminController',      'auth' =>false, 'action' => 'index'         ];
         $ar['admin.page']              = ['route' => '/admin/pages',                        'controller' => 'AdminController',      'auth' =>false, 'action' => 'page'          ];
         $ar['admin.cache']             = ['route' => '/admin/cache',                        'controller' => 'CacheController',      'auth' =>false, 'action' => 'index'         ];
-        $ar['admin.cache.delete']      = ['route' => '/admin/cache/delete',                 'controller' => 'CacheController',      'auth' =>false, 'action' => 'delete'         ];
-        $ar['admin.cache.show']        = ['route' => '/admin/cache/show',                   'controller' => 'CacheController',      'auth' =>false, 'action' => 'show'         ];
+        $ar['admin.cache.delete']      = ['route' => '/admin/cache/delete',                 'controller' => 'CacheController',      'auth' =>false, 'action' => 'delete'        ];
+        $ar['admin.cache.show']        = ['route' => '/admin/cache/show',                   'controller' => 'CacheController',      'auth' =>false, 'action' => 'show'          ];
         $ar['admin.page.content']      = ['route' => '/admin/page/content',                 'controller' => 'ContentController',    'auth' =>false, 'action' => 'index'         ];
         $ar['content.edit']            = ['route' => '/content/edit',                       'controller' => 'ContentController',    'auth' =>false, 'action' => 'edit'          ];
         $ar['content.update']          = ['route' => '/content/update',                     'controller' => 'ContentController',    'auth' =>false, 'action' => 'update'        ];
 
 
 
-        $ar['content.information']     = ['route' => '/content/information',                'controller' => 'InformationController',    'auth' =>false, 'action' => 'index'         ];
-        $ar['information.edit']        = ['route' => '/information/edit',                   'controller' => 'InformationController',    'auth' =>false,'action' => 'edit'           ];
-        $ar['information.update']      = ['route' => '/information/update',                 'controller' => 'InformationController',    'auth' =>false,'action' => 'update'         ];
+        $ar['content.information']     = ['route' => '/content/information',                'controller' => 'InformationController',    'auth' =>false,'action' => 'index'      ];
+        $ar['information.edit']        = ['route' => '/information/edit',                   'controller' => 'InformationController',    'auth' =>false,'action' => 'edit'       ];
+        $ar['information.update']      = ['route' => '/information/update',                 'controller' => 'InformationController',    'auth' =>false,'action' => 'update'     ];
 
         $ar['contact.index']           = ['route' => '/contact',                            'controller' => 'ContentController',    'action' => 'index'                         ];
         $ar['contact.delete']          = ['route' => '/contact/delete',                     'controller' => 'ContentController',    'action' => 'delete'                        ];
@@ -40,8 +39,6 @@ class Init extends Bootstrap
         $ar['auth.index']     = ['route' => '/auth',                                        'controller' => 'AuthController',       'action' => 'getLogin'                      ];
         $ar['auth.login']     = ['route' => '/auth/login',       'method'=>'POST',          'controller' => 'AuthController',       'action' => 'postLogin'                     ];
         $ar['auth.logout']    = ['route' => '/auth/logout',                                 'controller' => 'AuthController',       'action' => 'logout'                        ];
-
-
 
         $this->setRoutes($ar);
     }
