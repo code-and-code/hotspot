a1dc2224c00391d77ca34b1415716fa5ee65b118
Adriano Vianna <adriano_vianna@outlook.com>
2016-06-23 16:45:10 -0300

    fix

--- sql_hotspot.sql
+++ sql_hotspot.sql
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS `contacts` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
--- Copiando dados para a tabela hotspot.contacts: ~0 rows (aproximadamente)
+-- Copiando dados para a tabela hotspot.contacts: ~1 rows (aproximadamente)
 /*!40000 ALTER TABLE `contacts` DISABLE KEYS */;
 INSERT INTO `contacts` (`id`, `name`, `phone`, `email`, `acting`) VALUES
 	(4, 'Angelo Biscola', '41 363636363', 'angelo.biscola@codeandcode.com.br', NULL);
@@ -36,7 +36,7 @@ INSERT INTO `contacts` (`id`, `name`, `phone`, `email`, `acting`) VALUES
 CREATE TABLE IF NOT EXISTS `content` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
-  `content` varchar(255) NOT NULL,
+  `content` longtext NOT NULL,
   `flag` varchar(255) NOT NULL,
   `page_id` int(11) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
@@ -44,19 +44,18 @@ CREATE TABLE IF NOT EXISTS `content` (
   PRIMARY KEY (`id`),
   KEY `page_id` (`page_id`),
   CONSTRAINT `page_id` FOREIGN KEY (`page_id`) REFERENCES `pages` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 
--- Copiando dados para a tabela hotspot.content: ~8 rows (aproximadamente)
+-- Copiando dados para a tabela hotspot.content: ~7 rows (aproximadamente)
 /*!40000 ALTER TABLE `content` DISABLE KEYS */;
 INSERT INTO `content` (`id`, `name`, `content`, `flag`, `page_id`, `created_at`, `updated_at`) VALUES
-	(1, 'banner', '', 'banner', 1, '2016-06-13 16:44:14', '2016-06-15 18:52:13'),
-	(2, 'a_hotspot', '<p>A Hotspot foi desenvolvida para atuar no mercado de Coworking. Trabalha de forma inovadora, s&eacute;ria e comprometida. Sua localiza&ccedil;&atilde;o &eacute; privilegiada em Curitiba.</p><p>Possui agrad&aacute;vel ambiente de trabalho com salas equip', 'sobre', 2, '2016-06-13 16:44:24', '2016-06-16 17:04:59'),
-	(3, 'missao', '<p>Oferecer, agrad&aacute;vel ambiente, excelente estrutura, maior custo benef&iacute;cio e suporte t&eacute;cnico necess&aacute;rio para o desenvolvimento do trabalho.</p>', 'missao', 2, '2016-06-13 16:44:27', '2016-06-15 17:25:41'),
-	(4, 'visao', '<p>Ser uma conceituada e respeitada empresa de Coworking em &acirc;mbito Nacional e internacional.</p>', 'visao', 2, '2016-06-13 16:44:28', '2016-06-15 17:25:53'),
-	(5, 'valores', '<p>Trabalhar com &eacute;tica, seriedade, produtividade, relacionamento, e apropriado investimento.</p>', 'valores', 2, '2016-06-13 16:44:30', '2016-06-15 17:09:19'),
-	(6, 'como_funciona', '<p>Modalidade em que o cliente contrata uma esta&ccedil;&atilde;o de trabalho e utiliza o coworking compartilhando com outros profissionais ambiente de trabalho. Dentro da mesma estrutura, profissionais de diversos ramos de atividade trabalham individualm', '', 3, '2016-06-13 16:44:31', '2016-06-15 17:11:56'),
-	(7, 'servicos', '', 'servicos', 1, '2016-06-15 10:59:37', NULL),
-	(9, 'vantagens', '', '', 1, NULL, NULL);
+	(1, 'Banner', '', 'banner', 1, '2016-06-13 16:44:14', '2016-06-23 19:09:21'),
+	(2, 'A Hotpost', '<p>A <strong>Hotspot </strong>foi desenvolvida para atuar no mercado de Coworking. Trabalha de forma inovadora, s&eacute;ria e comprometida. Sua localiza&ccedil;&atilde;o &eacute; privilegiada em Curitiba.</p><p>Possui agrad&aacute;vel ambiente de trabalho com salas equipadas, suporte t&eacute;cnico e uma equipe altamente qualificada.</p><p>&nbsp;</p><p>Daremos todo suporte que voc&ecirc; precisa para colocar em pr&aacute;tica seu projeto de trabalho e fazer com que seu sonho seja realidade. A Hotspot &eacute; formada por uma equipe de profissionais que ir&aacute; agregar valor aos servi&ccedil;os que iremos lhe oferecer. J&aacute; atuamos em diversos seguimentos, tais como, &aacute;rea jur&iacute;dica, tecnol&oacute;gica, cont&aacute;bil e comercial, que automaticamente sua empresa ser&aacute; inserida.</p><p>&nbsp;</p><p>Oferecemos um espa&ccedil;o de trabalho inovador, para profissionais inteligentes que tem interesse de reinventar sua forma de trabalho.</p><p>&nbsp;</p><p>O profissional que escolhe atuar em um ambiente de coworking, poder&aacute; focar sua energia e seu tempo no que &eacute; realmente importante para alavancar e potencializar sua atividade dentro do mercado, colocando diante de suas m&atilde;os a possibilidade de um retorno extraordin&aacute;rio.</p>', 'about', 2, '2016-06-13 16:44:24', '2016-06-23 19:11:50'),
+	(6, 'Como Funciona', '<p>\r\n						Coworking é a união de um grupo de profissionais inovadores e talentosos, que trabalham independente uma das outras, dividindo o mesmo espaço e mesmo endereço.\r\n						São pessoas que compartilham seus valores e conhecimento, criam sinergia entre elas que aumenta a motivação e consequentemente seus resultados.\r\n					</p>\r\n					<p>\r\n						O coworking oferece mais que uma mesa de trabalho, nele você encontra internet, telefone, sala de treinamento,\r\n						sala de reunião totalmente equipada, impressora multifuncional, ampla copa, armários, gavetas, banheiros.\r\n						Tem tudo que você precisa para desenvolver suas atividades.\r\n					</p>\r\n\r\n					<p>\r\n						Além disso, junto com coworking tem também a atividade de escritório fiscal e comercial,<br/> <strong>O QUE SÃO?</strong> <br/><br/>\r\n						São duas atividades que utiliza o endereço do coworking para ser de sua empresa.\r\n						Você escolhe o local para receber correspondências, atender clientes e telefone. A diferença é que o escritório fiscal você utilizará o nosso endereço, como o endereço fiscal de sua empresa.\r\n					</p>', 'cf', 3, '2016-06-13 16:44:31', '2016-06-15 17:11:56'),
+	(7, 'Servicos', '', 'services', 1, '2016-06-15 10:59:37', NULL),
+	(9, 'Vantagens', '', 'list', 1, NULL, NULL),
+	(12, 'Beneficios', 'Beneficios', 'beneficios', 2, NULL, NULL),
+	(14, 'Info Impresa', '', 'mvv', 2, NULL, NULL);
 /*!40000 ALTER TABLE `content` ENABLE KEYS */;
 
 
@@ -69,7 +68,7 @@ CREATE TABLE IF NOT EXISTS `galeries` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
--- Copiando dados para a tabela hotspot.galeries: ~0 rows (aproximadamente)
+-- Copiando dados para a tabela hotspot.galeries: ~1 rows (aproximadamente)
 /*!40000 ALTER TABLE `galeries` DISABLE KEYS */;
 INSERT INTO `galeries` (`id`, `name`, `created_at`, `updated_at`) VALUES
 	(1, 'sala_de_reuniao', '2016-06-13 16:56:51', NULL);
@@ -80,23 +79,52 @@ INSERT INTO `galeries` (`id`, `name`, `created_at`, `updated_at`) VALUES
 CREATE TABLE IF NOT EXISTS `information` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
-  `content` varchar(255) NOT NULL,
+  `content` longtext NOT NULL,
+  `icon` varchar(255) DEFAULT NULL,
   `content_id` int(11) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `cateory_id` (`content_id`),
   CONSTRAINT `cateory_id` FOREIGN KEY (`content_id`) REFERENCES `content` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=66 DEFAULT CHARSET=utf8;
 
--- Copiando dados para a tabela hotspot.information: ~3 rows (aproximadamente)
+-- Copiando dados para a tabela hotspot.information: ~33 rows (aproximadamente)
 /*!40000 ALTER TABLE `information` DISABLE KEYS */;
-INSERT INTO `information` (`id`, `title`, `content`, `content_id`, `created_at`, `updated_at`) VALUES
-	(3, 'Valores', 'Trabalhar com ética, seriedade, produtividade, relacionamento, e apropriado investimento.', 1, '2016-06-16 12:06:08', NULL),
-	(6, 'Visão', 'Ser uma conceituada e respeitada empresa de Coworking em âmbito Nacional e internacional.', 1, '2016-06-17 11:31:31', NULL),
-	(7, 'Missão', 'Oferecer, agradável ambiente, excelente estrutura, maior custo benefício e suporte técnico necessário para o desenvolvimento do trabalho.', 1, '2016-06-17 11:31:33', NULL),
-	(12, 'Escritório Compartilhado', 'Espaço compartilhando com outros profissionais.', 7, '2016-06-17 11:31:34', NULL),
-	(15, 'Sala para Reuniões', 'Estrutura completa para sua reunião.', 7, '2016-06-17 11:31:36', NULL);
+INSERT INTO `information` (`id`, `title`, `content`, `icon`, `content_id`, `created_at`, `updated_at`) VALUES
+	(1, 'Missão', '<p>Lorem ipsum pretium torquent magna dui eleifend nisl, non euismod varius congue pretium pellentesque, porttitor primis convallis vel scelerisque sit.</p>', 'Missão', 14, '2016-06-17 11:31:33', '2016-06-23 19:14:51'),
+	(2, 'Visão', 'Ser uma conceituada e respeitada empresa de Coworking em âmbito Nacional e internacional.', 'Visão', 14, '2016-06-17 11:31:31', NULL),
+	(3, 'Valores', 'Trabalhar com ética, seriedade, produtividade, relacionamento, e apropriado investimento.', 'Valores', 14, '2016-06-16 12:06:08', NULL),
+	(12, 'Escritório Compartilhado', '<p>Espa&ccedil;o compartilhando com outros profissionais.</p><p>Modalidade em que o cliente contrata uma esta&ccedil;&atilde;o de trabalho e utiliza o coworking compartilhando com outros profissionais ambiente de trabalho. Dentro da mesma estrutura, profissionais de diversos ramos de atividade trabalham individualmente. Servi&ccedil;os de telefonia, recep&ccedil;&atilde;o, caf&eacute;, &aacute;gua, limpeza e manuten&ccedil;&atilde;o est&atilde;o inclu&iacute;dos na mensalidade. Outros podem ser solicitados sob demanda como motoboy e correios.</p><p>Vantagens:</p><ul><li>Servi&ccedil;os de telefonia</li><li>Internet</li><li>Recep&ccedil;&atilde;o</li><li>Caf&eacute;</li><li>&Aacute;gua.</li><li>Limpeza e manuten&ccedil;&atilde;o</li><li>Material para escrit&oacute;rio</li><li>Ambiente climatizado</li></ul>', 'leaf', 7, '2016-06-17 11:31:34', '2016-06-21 17:36:30'),
+	(15, 'Sala para Reuniões', '<p>Estrutura completa para sua reuni&atilde;o.</p><p>As salas s&atilde;o alugadas por hora, per&iacute;odo, di&aacute;rio e mensal, possuem servi&ccedil;os de copa com caf&eacute; e &aacute;gua inclusos. Nossas salas s&atilde;o equipadas:</p><p>Vantagens:</p><ul><li>Suporte de v&iacute;deo;</li><li>&Aacute;udio confer&ecirc;ncia;</li><li>TV Flipchart para trabalhos did&aacute;ticos;</li><li>Telefone;</li><li>Internet;</li><li>Ambiente climatizado;</li><li>Recep&ccedil;&atilde;o;</li><li>Caf&eacute;;</li><li>&Aacute;gua;</li><li>Limpeza e manuten&ccedil;&atilde;o;</li></ul><p><strong>&nbsp;</strong></p><p>Coffe Break podem ser servidos conforme solicita&ccedil;&atilde;o do cliente, servi&ccedil;o terceirizado disponibilizado separadamente</p>', 'blackboard', 7, '2016-06-17 11:31:36', '2016-06-21 18:09:02'),
+	(18, 'Escritório Fiscal', '<p>Endere&ccedil;o fiscal para atender seus clientes.</p><p>O Escrit&oacute;rio Virtual proporciona a sua empresa uma imagem coorporativa de prest&iacute;gio sempre nos endere&ccedil;os mais nobres da cidade.</p><p>Al&eacute;m de reduzir custos operacionais. Permite que a empresa se concentre em sua atividade principal:</p><ul><li>Recepcionista;</li><li>Atendimento ao cliente;</li><li>Um telefone fixo exclusivo que &eacute; atendido com o nome da empresa do cliente (personalizado);</li><li>Endere&ccedil;o comercial;</li><li>Gerenciamento das correspond&ecirc;ncias;</li><li>Endere&ccedil;o fiscal;</li><li>Central de correspond&ecirc;ncia;</li></ul>', 'globe', 7, '2016-06-17 11:54:51', '2016-06-21 18:17:07'),
+	(20, 'Sala Privativa', '<p>Sala reservada para equipe ou at&eacute; 3 pessoas.</p><p>Nossa sala Privativa possui ambiente mais reservado, para uma empresa ou compartilhada com at&eacute; 3 pessoas. Banheiro exclusivo- Sala rica em arm&aacute;rios e gaveteiros.</p><ul><li>Sala Reservada;</li><li>Banheiro;</li><li>Arm&aacute;rio Grande;</li><li>Ambiente climatizado;</li><li>Internet;</li><li>Mesa com cadeira;</li><li>Servi&ccedil;os de telefonia;</li><li>Recep&ccedil;&atilde;o;</li><li>Caf&eacute;;</li><li>&Aacute;gua;</li><li>Material para escrit&oacute;rio;</li></ul><p>Outros podem ser solicitados conforme necessidade, como motoboy e correios (servi&ccedil;os a parte).</p>', 'lock', 7, '2016-06-17 11:55:27', '2016-06-21 18:11:23'),
+	(22, 'Escritório Comercial', '<p>Endere&ccedil;o comercial para atender seus clientes.</p><p>O endere&ccedil;o comercial ser&aacute; utilizado pelo contratante para ter um endere&ccedil;o em local nobre que receba suas correspond&ecirc;ncias, que poder&aacute; ser utilizado em seu material de marketing, em cart&otilde;es de visita, panfletos, site etc.. Incluindo os servi&ccedil;os:</p><ul><li>Recepcionista;</li><li>Atendimento telef&ocirc;nico;</li><li>Endere&ccedil;o comercial;</li><li>Gerenciamento das correspond&ecirc;ncias;</li><li>Envio de recado.</li></ul>', 'usd', 7, '2016-06-17 11:57:35', '2016-06-21 18:18:48'),
+	(23, 'Sala de treinamento', '<p>Sala estruturada para realizar seu treinamento, palestra ou curso.</p><p>Formato audit&oacute;rio, possui capacidade para 15 pessoas. As salas s&atilde;o alugadas por per&iacute;odo e dia, possuem servi&ccedil;os de copa com caf&eacute; e &aacute;gua inclusos. Nossas salas s&atilde;o equipadas com:</p><ul><li>Suporte de v&iacute;deo e &aacute;udio confer&ecirc;ncia</li><li>TV;</li><li>Flipchart para trabalhos did&aacute;ticos,</li><li>Internet;</li><li>Ar condicionado.</li><li>Jardim de inverno para deixar o ambiente agrad&aacute;vel</li></ul><p>Coffe Break podem ser servidos conforme solicita&ccedil;&atilde;o do cliente, servi&ccedil;o (terceirizado) disponibilizado separadamente.</p>', 'cog', 7, '2016-06-17 11:57:36', '2016-06-21 18:15:06'),
+	(24, 'Lista Vantagens 1', '<p>Escritorio em uma &aacute;rea nobre com baixo custo.</p>', '', 9, '2016-06-17 12:10:56', '2016-06-23 19:25:04'),
+	(25, 'Lista Vantagens 2', 'Mobília e estrutura completa.', '', 9, '2016-06-17 12:10:57', NULL),
+	(27, 'Lista Vantagens 3', 'Recepção, cozinha, sala de reuniões.', '', 9, NULL, NULL),
+	(29, 'Lista Vantagens 4', 'Várias empresas ao seu lado.', '', 9, NULL, NULL),
+	(31, 'Lista Vantagens 5', 'Linhas telefonicas', '', 9, NULL, NULL),
+	(33, 'Lista Vantagens 6', 'Conforto e privacidade.', '', 9, NULL, NULL),
+	(34, 'Missão', '<p>Oferecer, agrad&aacute;vel ambiente, excelente estrutura, maior custo benef&iacute;cio e suporte t&eacute;cnico necess&aacute;rio para o desenvolvimento do trabalho.</p>', NULL, 1, NULL, '2016-06-23 19:08:27'),
+	(35, 'Visão', '<p>Ser uma conceituada e respeitada empresa de Coworking em &acirc;mbito Nacional e internacional.</p>', NULL, 1, NULL, '2016-06-23 19:07:44'),
+	(36, 'Valores', '<p>Trabalhar com &eacute;tica, seriedade, produtividade, relacionamento, e apropriado investimento.</p>', NULL, 1, NULL, '2016-06-23 19:08:08'),
+	(39, 'Lista Beneficios 1', 'Menor custo operacional.', 'list', 12, NULL, NULL),
+	(41, 'Lista Beneficios 2', 'Ambiente empresarial moderno.', 'list', 12, NULL, NULL),
+	(43, 'Lista Beneficios 3', 'Investimento inicial zero.', 'list', 12, NULL, NULL),
+	(45, 'Lista Beneficios 4', 'Agilidade nas atividades operacionais de um escritório.', 'list', 12, NULL, NULL),
+	(47, 'Lista Beneficios 5', 'Foco na atividade fim do seu negócio.', 'list', 12, NULL, NULL),
+	(48, 'Lista Beneficios 6', 'Networking profissional.', 'list', 12, NULL, NULL),
+	(50, 'Lista Beneficios 7', 'Networking pessoal.', 'list', 12, NULL, NULL),
+	(52, 'Lista Beneficios 8', 'Grupo de empresas que poderá lhe auxiliar.', 'list', 12, NULL, NULL),
+	(58, 'Lista Busca 1', 'Profissionais que querem ampliar seu networking.', NULL, 6, NULL, NULL),
+	(59, 'Lista Busca 2', 'Sair de seu home office.', NULL, 6, NULL, NULL),
+	(60, 'Lista Busca 3', 'Diminuir as despesas com escritórios.', NULL, 6, NULL, NULL),
+	(62, 'Lista Busca 4', 'Startups.', NULL, 6, NULL, NULL),
+	(63, 'Lista Busca 5', 'Expandir seus negócios.', NULL, 6, NULL, NULL),
+	(64, 'Lista Busca 6', 'Ter rápido retorno financeiro.', NULL, 6, NULL, NULL),
+	(65, 'Lista Busca 7', 'Evitar erros comuns.', NULL, 6, NULL, NULL);
 /*!40000 ALTER TABLE `information` ENABLE KEYS */;
 
 
@@ -104,7 +132,7 @@ INSERT INTO `information` (`id`, `title`, `content`, `content_id`, `created_at`,
 CREATE TABLE IF NOT EXISTS `pages` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `view` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `cache` varchar(255) CHARACTER SET utf8 NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
@@ -112,11 +140,11 @@ CREATE TABLE IF NOT EXISTS `pages` (
 
 -- Copiando dados para a tabela hotspot.pages: ~4 rows (aproximadamente)
 /*!40000 ALTER TABLE `pages` DISABLE KEYS */;
-INSERT INTO `pages` (`id`, `name`, `view`, `created_at`, `updated_at`) VALUES
-	(1, 'inicio', 'index.phtml', '2016-06-13 16:54:02', NULL),
-	(2, 'hotspot_coworking', '', '2016-06-13 16:53:59', NULL),
-	(3, 'como_funciona', '', '2016-06-13 16:54:01', NULL),
-	(4, 'imagens', '', '2016-06-13 16:54:02', NULL);
+INSERT INTO `pages` (`id`, `name`, `cache`, `created_at`, `updated_at`) VALUES
+	(1, 'inicio', 'index', '2016-06-13 16:54:02', NULL),
+	(2, 'hotspot_coworking', 'about', '2016-06-13 16:53:59', NULL),
+	(3, 'como_funciona', 'works', '2016-06-13 16:54:01', NULL),
+	(4, 'imagens', 'images', '2016-06-13 16:54:02', NULL);
 /*!40000 ALTER TABLE `pages` ENABLE KEYS */;
 
 
@@ -131,7 +159,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
--- Copiando dados para a tabela hotspot.users: ~0 rows (aproximadamente)
+-- Copiando dados para a tabela hotspot.users: ~1 rows (aproximadamente)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` (`id`, `email`, `name`, `password`, `created_at`, `updated_at`) VALUES
 	(1, 'suporte@codeandcode.com.br', 'suporte', 'admin', '2016-06-13 16:56:59', NULL);
