ac83bed9911964effb47fa982b2f121a9d3af55e
Angelo Biscola <angelo.biscola@gmail.com>
2016-06-24 12:06:25 -0300

    fix route

--- App/Init.php
+++ App/Init.php
@@ -14,35 +14,34 @@ class Init extends Bootstrap
         $ar['site.images']            = ['route' => '/imagens',                             'controller' => 'SiteController',       'action' => 'images'                        ];
 
 
-
-
-        $ar['admin']                   = ['route' => '/admin',                              'controller' => 'AdminController',      'auth' =>false, 'action' => 'index'         ];
-        $ar['admin.page']              = ['route' => '/admin/pages',                        'controller' => 'AdminController',      'auth' =>false, 'action' => 'page'          ];
-        $ar['admin.cache']             = ['route' => '/admin/cache',                        'controller' => 'CacheController',      'auth' =>false, 'action' => 'index'         ];
-        $ar['admin.cache.delete']      = ['route' => '/admin/cache/delete',                 'controller' => 'CacheController',      'auth' =>false, 'action' => 'delete'         ];
-        $ar['admin.cache.show']        = ['route' => '/admin/cache/show',                   'controller' => 'CacheController',      'auth' =>false, 'action' => 'show'         ];
-        $ar['admin.page.content']      = ['route' => '/admin/page/content',                 'controller' => 'ContentController',    'auth' =>false, 'action' => 'index'         ];
-        $ar['content.edit']            = ['route' => '/content/edit',                       'controller' => 'ContentController',    'auth' =>false, 'action' => 'edit'          ];
-        $ar['content.update']          = ['route' => '/content/update',                     'controller' => 'ContentController',    'auth' =>false, 'action' => 'update'        ];
-
-
-
-        $ar['content.information']     = ['route' => '/content/information',                'controller' => 'InformationController',    'auth' =>false, 'action' => 'index'         ];
-        $ar['information.edit']        = ['route' => '/information/edit',                   'controller' => 'InformationController',    'auth' =>false,'action' => 'edit'           ];
-        $ar['information.update']      = ['route' => '/information/update',                 'controller' => 'InformationController',    'auth' =>false,'action' => 'update'         ];
-
-        $ar['contact.index']           = ['route' => '/contact',                            'controller' => 'ContentController',    'action' => 'index'                         ];
-        $ar['contact.delete']          = ['route' => '/contact/delete',                     'controller' => 'ContentController',    'action' => 'delete'                        ];
-
-        $ar['photo.create']            = ['route' => '/admin/photo/create',                       'controller' => 'PhotoController',      'auth' =>false,'action' => 'create'         ];
-        $ar['photo.store']             = ['route' => '/admin/photo/store','method'=>'POST',       'controller' => 'PhotoController',      'auth' =>false,'action' => 'store'          ];
-
-
         $ar['auth.index']     = ['route' => '/auth',                                        'controller' => 'AuthController',       'action' => 'getLogin'                      ];
         $ar['auth.login']     = ['route' => '/auth/login',       'method'=>'POST',          'controller' => 'AuthController',       'action' => 'postLogin'                     ];
         $ar['auth.logout']    = ['route' => '/auth/logout',                                 'controller' => 'AuthController',       'action' => 'logout'                        ];
 
 
+
+        $ar['admin']                   = ['route' => '/admin',                              'controller' => 'AdminController',      'auth' =>true, 'action' => 'index'         ];
+        $ar['admin.page']              = ['route' => '/admin/pages',                        'controller' => 'AdminController',      'auth' =>true, 'action' => 'page'          ];
+
+
+        $ar['admin.cache']             = ['route' => '/admin/cache',                        'controller' => 'CacheController',      'auth' =>true, 'action' => 'index'            ];
+        $ar['admin.cache.show']        = ['route' => '/admin/cache/show',                   'controller' => 'CacheController',      'auth' =>true, 'action' => 'show'             ];
+        $ar['admin.cache.delete']      = ['route' => '/admin/cache/delete',                 'controller' => 'CacheController',      'auth' =>true, 'action' => 'delete'           ];
+
+
+        $ar['admin.content.index']      = ['route' => '/admin/content/index',                   'controller' => 'ContentController',    'auth' =>true, 'action' => 'index'         ];
+        $ar['admin.content.edit']       = ['route' => '/admin/content/edit',                    'controller' => 'ContentController',    'auth' =>true, 'action' => 'edit'          ];
+        $ar['admin.content.update']     = ['route' => '/admin/content/update', 'method'=>'POST','controller' => 'ContentController',    'auth' =>true, 'action' => 'update'        ];
+
+
+
+        $ar['admin.information.index']     = ['route' => '/admin/information',                        'controller' => 'InformationController',    'auth' =>true,'action' => 'index'  ];
+        $ar['admin.information.edit']      = ['route' => '/admin/information/edit',                   'controller' => 'InformationController',    'auth' =>true,'action' => 'edit'   ];
+        $ar['admin.information.update']    = ['route' => '/admin/information/update','method'=>'POST','controller' => 'InformationController',    'auth' =>true,'action' => 'update' ];
+
+
+        $ar['admin.photo.create']            = ['route' => '/admin/photo/create',                       'controller' => 'PhotoController',      'auth' =>true,'action' => 'create'         ];
+        $ar['admin.photo.store']             = ['route' => '/admin/photo/store','method'=>'POST',       'controller' => 'PhotoController',      'auth' =>true,'action' => 'store'          ];
 
         $this->setRoutes($ar);
     }
